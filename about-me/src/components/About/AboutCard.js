e2dd7dad57ac494cdb02688e8837342eb1946b3d
Mrigank-Khandelwal <mrigankkhandelwal300@gmail.com>
2023-11-24 10:18:26 -0500

    creating dev branch to keep development branch and deploying branch separate

---
+++ about-me/src/components/About/AboutCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+
+            I am currently living in <span className="purple"> Boston, Massachusetts </span>
+            where I am pursuing my Masters Degree in Computer Science from Northeastern University.
+            <br />
+    
+            <br />
+I love travelling and I am always down for a hastily planned weekend getaway.
+
+          </p>
+       
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Do one thing every day that scares you."{" "}
+          </p>
+          <footer className="blockquote-footer">Eleanor Roosevelt</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
